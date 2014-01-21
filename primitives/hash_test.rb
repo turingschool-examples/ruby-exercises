60c683773cff15a207b7d1cedd80adf317ecd76d
Katrina Owen <katrina.owen@gmail.com>
2014-01-20 18:04:38 -0700

    Add assignment/access to hash test

--- primitives/hash_test.rb
+++ primitives/hash_test.rb
@@ -13,6 +13,22 @@ class HashTest < Minitest::Homework
     maybe boolean
   end
 
+  def test_access
+    ages = {16 => ["Alice", "Bob"], 24 => ["Charlie", "Dave"], 37 => ["Eve"]}
+    assert_equal __, ages[16]
+    assert_equal __, ages[37]
+  end
+
+  def test_assignment
+    stuff = {"a" => ["aardvark", "apple"], "b" => ["butcher", "ballerina"]}
+    stuff["c"] = ["cook", "caramel candy"]
+    assert_equal __, stuff["c"]
+    stuff["b"] = "brandy"
+    assert_equal __, stuff["b"]
+    stuff["a"] = stuff["a"] + ["apricot"]
+    assert_equal __, stuff["a"]
+  end
+
   def test_keys
     assert_equal __, {}.keys
     assert_equal __, {1 => "one", 2 => "two"}.keys
