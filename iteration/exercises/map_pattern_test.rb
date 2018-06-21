e8e255e22f6e71b316a05e7b5703272c7f7bc84e
Brian Zanti <brianzanti@gmail.com>
2018-06-21 13:23:07 -0600

    adds map

--- iteration/exercises/map_pattern_test.rb
+++ iteration/exercises/map_pattern_test.rb
@@ -1,10 +1,9 @@
-gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 
 class MapPatternTest < Minitest::Test
 
-  def test_capitalize
+  def test_1
     names = ["alice", "bob", "charlie"]
     capitalized_names = []
     names.each do |name|
@@ -13,56 +12,131 @@ class MapPatternTest < Minitest::Test
     assert_equal ["Alice", "Bob", "Charlie"], capitalized_names
   end
 
-  def test_doubles
+  def test_2
+    family = {
+      mother: "alice",
+      father: "bob",
+      brother: "charlie"
+    }
+    capitalized_family = {}
+    family.each do |relationship, name|
+      capitalized_family[relationship] = name.capitalize
+    end
+    expected = {
+      mother: "Alice",
+      father: "Bob",
+      brother: "Charlie"
+    }
+    assert_equal expected, capitalized_family
+  end
+
+  def test_3
     numbers = [1, 2, 3, 4, 5]
     doubles = []
     numbers.each do |number|
-      # Your code goes here
+      # Your Code Here
     end
     assert_equal [2, 4, 6, 8, 10], doubles
   end
 
-  def test_squares
-    skip
+  def test_4
+    numbers = {
+      one: 1,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5
+    }
+    doubles = {}
+    numbers.each do |name, number|
+      # Your Code Here
+    end
+    expected = {
+      one: 2,
+      two: 4,
+      three: 6,
+      four: 8,
+      five: 10
+    }
+    assert_equal expected, doubles
+  end
+
+  def test_5
     numbers = [1, 2, 3, 4, 5]
     squares = []
-    # Your code goes here
+    # Your Code Here
+
     assert_equal [1, 4, 9, 16, 25], squares
   end
 
-  def test_lengths
-    skip
+  def test_6
+    numbers = {
+      one: 1,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5
+    }
+    squares = {}
+    # Your Code Here
+
+    expected = {
+      one: 1,
+      two: 4,
+      three: 9,
+      four: 16,
+      five: 25
+    }
+    assert_equal expected, squares
+  end
+
+  def test_7
     names = ["alice", "bob", "charlie", "david", "eve"]
-    # Your code goes here
+
     assert_equal [5, 3, 7, 5, 3], lengths
   end
 
-  def test_normalize_zip_codes
-    skip
-    numbers = [234, 10, 9119, 38881]
-    # Your code goes here
-    assert_equal ["00234", "00010", "09119", "38881"], zip_codes
+  def test_8
+    family = {
+      mother: "alice",
+      father: "bob",
+      brother: "charlie",
+      uncle: "david",
+      sister: "eve"
+    }
+
+    expected = {
+      mother: 5,
+      father: 3,
+      brother: 7,
+      uncle: 5,
+      sister: 3
+    }
+    assert_equal expected, lengths
   end
 
-  def test_backwards
-    skip
+  def test_9
     names = ["alice", "bob", "charlie", "david", "eve"]
-    # Your code goes here
+
     assert_equal ["ecila", "bob", "eilrahc", "divad", "eve"], backwards
   end
 
-  def test_words_with_no_vowels
-    skip
-    words = ["green", "sheep", "travel", "least", "boat"]
-    # Your code goes here
-    assert_equal ["grn", "shp", "trvl", "lst", "bt"], without_vowels
-  end
+  def test_10
+    family = {
+      mother: "alice",
+      father: "bob",
+      brother: "charlie",
+      uncle: "david",
+      sister: "eve"
+    }
 
-  def test_trim_last_letter
-    skip
-    animals = ["dog", "cat", "mouse", "frog", "platypus"]
-    # Your code goes here
-    assert_equal ["do", "ca", "mous", "fro", "platypu"], trimmed
+    expected = {
+      mother: "ecila",
+      father: "bob",
+      brother: "eilrahc",
+      uncle: "divad",
+      sister: "eve"
+    }
+    assert_equal expected, backwards
   end
-
 end
