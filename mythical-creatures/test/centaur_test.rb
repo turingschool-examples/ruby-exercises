f5bc5ada0b4b9ad1267a736679e500ec725279e2
Sal Espinosa <sespinos@gmail.com>
2018-08-10 09:34:03 -0600

    Update readme and reorganize files

--- mythical-creatures/test/centaur_test.rb
+++ mythical-creatures/test/centaur_test.rb
@@ -1,7 +1,7 @@
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative 'centaur'
+require_relative '../lib/centaur'
 
 class CentaurTest < Minitest::Test
   def test_it_has_a_name
