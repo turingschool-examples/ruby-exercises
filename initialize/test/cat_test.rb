69a666edd5e949c2a3d0efb994640414a01f0084
Aizar <aizar.cabrera@icloud.com>
2018-09-06 16:59:20 -0600

    add class for cat and make test pass

--- initialize/test/cat_test.rb
+++ initialize/test/cat_test.rb
@@ -4,17 +4,17 @@ require './lib/cat'
 
 class CatTest < Minitest::Test
   def test_it_exists
-    carlos = Cat.new("Carlos")
-    assert_instance_of Cat, carlos
+    boots = Cat.new("Boots")
+    assert_instance_of Cat, boots
   end
 
   def test_it_has_a_name
-    carlos = Cat.new("Carlos")
-    assert_equal "Carlos", carlos.name
+    boots = Cat.new("Boots")
+    assert_equal "Boots", boots.name
   end
 
   def test_it_has_a_sound
-    carlos = Cat.new("Carlos")
-    assert_equal "meow", carlos.sound
+    boots = Cat.new("Boots")
+    assert_equal "meow", boots.sound
   end
 end
