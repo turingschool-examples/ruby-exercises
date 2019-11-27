1e7d9e351473154adce1920f98840f9052cf7de9
Katie Williams <kawilliams8@gmail.com>
2019-11-27 08:25:53 -0700

    Pass test_11 chomp('e')

--- data-types/strings/strings.rb
+++ data-types/strings/strings.rb
@@ -100,10 +100,9 @@ class StringTest < Minitest::Test
   end
 
   def test_11
-    skip
     rhyme = "eeny, meeny, miny, moe"
     # In place of the line below, call a method to achieve the expected output.
-    actual = rhyme._____
+    actual = rhyme.chomp('e')
     expected = "ny, mny, miny, mo"
 
     assert_equal expected, actual
