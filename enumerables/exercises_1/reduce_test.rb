f677d45e680cdd3b13aa29276b4ee44bf248d3e3
Jori Peterson <joripeterson@gmail.com>
2019-12-08 17:15:50 -0700

    Solve reduce tests

--- enumerables/exercises_1/reduce_test.rb
+++ enumerables/exercises_1/reduce_test.rb
@@ -15,48 +15,52 @@ class ReduceTest < Minitest::Test
   def test_subtract_list_of_numbers
     numbers = [28, 12, 38, 1, 91]
     result = numbers.reduce(0) do |difference, number|
-      # Your code goes here
+      difference - number
     end
     assert_equal -170, result
   end
 
   def test_multiply_list_of_numbers
-    skip
     numbers = [2, 3, 5, 7]
-    # initial value is 1
-    # Your code goes here
+    result = numbers.reduce(1) do |multiply, number|
+      multiply * number
+    end
     assert_equal 210, result
   end
 
   def test_capitalize_keywords_in_phrase_one_fish_two_fish_red_fish_blue_fish
-    skip
     keywords = ["fish", "blue"]
     # initial value is 'one fish two fish red fish blue fish'
-    # Your code goes here
+    result = keywords.reduce('one fish two fish red fish blue fish') do |phrase, word|
+      phrase.gsub!(word, word.upcase)
+    end
     assert_equal 'one FISH two FISH red FISH BLUE FISH', result
   end
 
   def test_divide_560_by_a_bunch_of_numbers
-    skip
     numbers = [2, 2, 2, 5, 7]
     # initial value is 560
-    # Your code goes here
+    result = numbers.reduce(560) do |value, number|
+      value / number
+    end
     assert_equal 2, result
   end
 
   def test_subtract_smallest_values_from_100
-    skip
     elements = [[8, 5, 3], [1, 9, 11], [4, 7, 2], [19, 34, 6]]
     # initial value is 100
-    # Your code goes here
+    result = elements.reduce(100) do |value, element|
+      value - element.min
+    end
     assert_equal 88, result
   end
 
   def test_add_all_the_second_values_together
-    skip
     elements = [["a", 1], ["b", 9], ["c", 21]]
     # initial value is 0
-    # Your code goes here
+    result = elements.reduce(0) do |value, element|
+      value + element[1]
+    end  
     assert_equal 31, result
   end
 
