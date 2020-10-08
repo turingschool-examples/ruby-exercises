f2ac55ad95487ab0ad90755bd54dbe2270fa0583
Michael Dao <mike@turing.io>
2020-10-07 19:17:16 -0600

    updates mythical creatures

---
+++ mythical-creatures/spec/ogre_spec.rb
@@ -0,0 +1,98 @@
+require './spec/spec-helper'
+require './lib/ogre'
+
+RSpec.describe Ogre do
+  it 'has a name' do
+    ogre = Ogre.new('Brak')
+    expect(ogre.name).to eq('Brak')
+  end
+
+  it 'lives somewhere by default' do
+    ogre = Ogre.new('Brak')
+    expect(ogre.home).to eq('Swamp')
+  end
+
+  it 'doesnt have to live in a swamp' do
+    ogre = Ogre.new('Brak', 'Castle')
+
+    expect(ogre.home).to eq('Castle')
+  end
+
+  it 'can meets humans' do
+    ogre = Ogre.new('Brak')
+    human = Human.new
+    expect(human.name).to eq('Jane')
+
+    ogre.encounter(human)
+
+    expect(human.encounter_counter).to eq(1)
+  end
+
+  it 'is noticed by humans every third encounter' do
+    ogre = Ogre.new('Brak')
+    human = Human.new
+
+    ogre.encounter(human)
+    ogre.encounter(human)
+    expect(human.notices_ogre?).to be false
+
+    ogre.encounter(human)
+
+    expect(human.notices_ogre?).to be true
+  end
+
+  it 'is noticed by humans the sixth time' do
+    ogre = Ogre.new('Brak')
+    human = Human.new
+
+    6.times { ogre.encounter(human) }
+
+    expect(human.notices_ogre?).to be true
+  end
+
+  it 'can swing a club' do
+    ogre = Ogre.new('Brak')
+    human = Human.new
+
+    ogre.swing_at(human)
+
+    expect(ogre.swings).to eq(1)
+  end
+
+  it 'swings its club when noticed by a human' do
+    ogre = Ogre.new('Brak')
+    human = Human.new
+    ogre.encounter(human)
+
+    expect(ogre.swings).to eq(0)
+
+    ogre.encounter(human)
+    ogre.encounter(human)
+
+    expect(ogre.swings).to eq(1)
+    expect(human.notices_ogre?).to be true
+  end
+
+  it 'hits the human every second time it swings' do
+    ogre = Ogre.new('Brak')
+    human = Human.new
+
+    6.times { ogre.encounter(human) }
+
+    expect(ogre.encounter_counter).to eq(6)
+    expect(ogre.swings).to eq(2)
+    expect(human.knocked_out?).to be true
+  end
+
+  it 'apologizes and the human wakes up' do
+    ogre = Ogre.new('Brak')
+    human = Human.new
+
+    6.times { ogre.encounter(human) }
+
+    expect(human.knocked_out?).to be true
+
+    ogre.apologize(human)
+    expect(human.knocked_out?).to be false
+  end
+end
---
+++ mythical-creatures/spec/pirate_spec.rb
@@ -0,0 +1,51 @@
+require 'rspec'
+require './lib/pirate'
+
+RSpec.describe Pirate do
+  it 'has a name' do
+    pirate = Pirate.new('Jane')
+    expect(pirate.name).to eq('Jane')
+  end
+
+  it 'can have a different name' do
+    pirate = Pirate.new('Blackbeard')
+    expect(pirate.name).to eq('Blackbeard')
+  end
+
+  it 'is a scallywag by default' do
+    pirate = Pirate.new('Jane')
+    expect(pirate.job).to eq('Scallywag')
+  end
+
+  it 'is not always a scallywag' do
+    pirate = Pirate.new('Jack', 'cook')
+    expect(pirate.job).to eq('cook')
+  end
+
+  it 'is not cursed by default' do
+    pirate = Pirate.new('Jack')
+
+    expect(pirate.cursed?).to be false
+
+    pirate.commit_heinous_act
+    expect(pirate.cursed?).to be false
+
+    pirate.commit_heinous_act
+    expect(pirate.cursed?).to be false
+
+    pirate.commit_heinous_act
+    expect(pirate.cursed?).to be true
+  end
+
+  it 'has a booty' do
+    # create a pirate
+    # check that the pirate starts with 0 booty
+  end
+
+  it 'gets 100 booty for robbing a ship' do
+    # create a pirate
+    # rob some ships
+    # check that the pirate got 100 booty for each ship it robbed
+  end
+
+end
