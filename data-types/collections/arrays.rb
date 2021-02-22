a50be680707e995594088fe53529672e1cc6461b
Norma Lopez <norma_l304@yahoo.com>
2021-02-22 15:34:26 -0700

    Insert new element at index 1 for array in test_11 using .insert(); test passsing

--- data-types/collections/arrays.rb
+++ data-types/collections/arrays.rb
@@ -140,11 +140,10 @@ class ArraysTest < Minitest::Test
   end
 
   def test_11
-    skip
     furniture = ["couch", "bed", "table"]
     # Call a method on the furniture variable to add the element
     # "dresser" in between the elements "couch" and "bed"
-    new_furniture = furniture
+    new_furniture = furniture.insert(1, 'dresser')
     assert_equal ["couch", "dresser", "bed", "table"], new_furniture
   end
 
