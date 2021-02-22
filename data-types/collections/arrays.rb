4bb092a5839c5fa297a41cf8d752d1bdbc45def2
Norma Lopez <norma_l304@yahoo.com>
2021-02-22 15:17:48 -0700

    Use .flatten on array in test_8 to remove nesting; test passing

--- data-types/collections/arrays.rb
+++ data-types/collections/arrays.rb
@@ -113,10 +113,9 @@ class ArraysTest < Minitest::Test
   end
 
   def test_8
-    skip
     pokemon = [["Squirtle", "Charmander", "Bulbasaur"], ["Pikachu"], "Staryu"]
     # Call a method on the pokemon variable to remove the nesting
-    pokemon = pokemon
+    pokemon = pokemon.flatten
     assert_equal ["Squirtle", "Charmander", "Bulbasaur", "Pikachu", "Staryu"], pokemon
   end
 
