8dd15ff61ce43f9f9eb7e3b184a31a4adede9441
Norma Lopez <norma_l304@yahoo.com>
2021-02-22 15:21:32 -0700

    Delete element from array using .delete for test_10; test passing

--- data-types/collections/arrays.rb
+++ data-types/collections/arrays.rb
@@ -133,10 +133,9 @@ class ArraysTest < Minitest::Test
   end
 
   def test_10
-    skip
     vowels = ["a", "e", "y", "i", "o", "u", "y"]
     # Call a method on the vowels variable to remove "y"
-
+    vowels.delete('y')
     assert_equal ["a", "e", "i", "o", "u"], vowels
   end
 
