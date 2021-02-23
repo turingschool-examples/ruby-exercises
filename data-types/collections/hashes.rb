aab3cacdbfc2194502ce3d648cceb9deeb95008f
Norma Lopez <norma_l304@yahoo.com>
2021-02-23 14:00:27 -0700

    Query hash to retrieve value for specific key for test_6

--- data-types/collections/hashes.rb
+++ data-types/collections/hashes.rb
@@ -63,7 +63,7 @@ class HashesTest < Minitest::Test
     }
     # Using the books hash defined above,
     # access the value "Grapes of Wrath"  in the line below
-    grapes = ______
+    grapes = books["John Steinbeck"]
     assert_equal "Grapes of Wrath", grapes
   end
 
