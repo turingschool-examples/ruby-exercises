5e3998e38a3a93959249382b03d1d04eca253690
Norma Lopez <norma_l304@yahoo.com>
2021-02-23 13:03:08 -0700

    Use .each and accumulator to create new array with all words from original array that end in the letter 'e'

--- enumerables/exercises_1/select_pattern_test.rb
+++ enumerables/exercises_1/select_pattern_test.rb
@@ -41,9 +41,11 @@ class SelectPatternTest < Minitest::Test
   end
 
   def test_pick_words_ending_in_e
-    skip
     words = ["are", "you", "strike", "thinking", "belt", "piece", "warble", "sing", "pipe"]
-    # Your code goes here
+    selected = []
+    words.each do |word|
+      selected << word if word[-1] == 'e'
+    end
     assert_equal ["are", "strike", "piece", "warble", "pipe"], selected
   end
 
