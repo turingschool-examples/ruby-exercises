b6fac2d10721c04ba8d085e6ce1792e5860363a0
Norma Lopez <norma_l304@yahoo.com>
2021-02-23 14:03:09 -0700

    Change value for specific key in test_8

--- data-types/collections/hashes.rb
+++ data-types/collections/hashes.rb
@@ -93,7 +93,7 @@ class HashesTest < Minitest::Test
     # Using the books hash defined above,
     # change the value associated with
     # "John Steinbeck" to "Of Mice and Men"
-    ______
+    books["John Steinbeck"] = "Of Mice and Men"
     expected = {
       "John Steinbeck" => "Of Mice and Men",
       "Harper Lee" => "To Kill a Mockingbird"
