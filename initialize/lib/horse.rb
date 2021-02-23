2add098053d36bdf01a70dfec0eb54b1cbd5f30b
Norma Lopez <norma_l304@yahoo.com>
2021-02-22 22:00:06 -0700

    Add #add_to_diet() to add elements to diet array

--- initialize/lib/horse.rb
+++ initialize/lib/horse.rb
@@ -1,8 +1,12 @@
 class Horse
-  attr_reader :name, :diet 
+  attr_reader :name, :diet
 
   def initialize(name)
     @name = name
     @diet = []
+  end
+
+  def add_to_diet(food)
+    @diet << food
   end
 end
