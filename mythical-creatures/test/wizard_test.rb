78becc7aaa7e7afd762f85ba80b929d180eb893f
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-13 14:41:15 -0800

    Unskip test, add last two blocks inside tests

--- mythical-creatures/test/wizard_test.rb
+++ mythical-creatures/test/wizard_test.rb
@@ -10,49 +10,49 @@ class WizardTest < Minitest::Test
   end
 
   def test_can_have_different_name
-    skip
+    # skip
     wizard = Wizard.new("Alex")
     assert_equal "Alex", wizard.name
   end
 
   def test_is_bearded_by_default
-    skip
+    # skip
     wizard = Wizard.new("Ben")
     assert_equal true, wizard.bearded?
   end
 
   def test_is_not_always_bearded
-    skip
+    # skip
     wizard = Wizard.new("Valerie", bearded: false)
     assert_equal false, wizard.bearded?
   end
 
   def test_has_root_powers
-    skip
+    # skip
     wizard = Wizard.new("Sarah", bearded: false)
     assert_equal "sudo chown ~/bin", wizard.incantation("chown ~/bin")
   end
 
   def test_has_lots_of_root_powers
-    skip
+    # skip
     wizard = Wizard.new("Rob", bearded: false)
     assert_equal "sudo rm -rf /home/mirandax", wizard.incantation("rm -rf /home/mirandax")
   end
 
   def test_starts_rested
-    skip
+    # skip
     wizard = Wizard.new("Leigh", bearded: false)
     assert_equal true, wizard.rested?
   end
 
   def test_can_cast_spells
-    skip
+    # skip
     wizard = Wizard.new("Nick", bearded: false)
     assert_equal "MAGIC MISSILE!", wizard.cast
   end
 
   def test_gets_tired_after_casting_three_spells
-    skip
+    # skip
     wizard = Wizard.new("Brittany", bearded: false)
     wizard.cast
     wizard.cast
