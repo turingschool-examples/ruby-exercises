dc392a7eab5f0bcf72e916981470e852ecb443c0
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-12 16:29:32 -0800

    Add spec

---
+++ mythical-creatures/spec/creature_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require_relative '../lib/creature'
+
+RSpec.describe "Mytest", :type => :request do
+
+  describe "check smth" do
+    it "should be eq" do
+      expect(3).to   eq(3)
+    end
+
+  end
+
+
+end
