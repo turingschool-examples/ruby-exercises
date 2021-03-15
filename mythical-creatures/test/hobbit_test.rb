07c6a3129a7699e7efe1fbf8eacd88b98c28ffb0
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-15 13:07:08 -0700

    Unskip/build tests to drive method creation

--- mythical-creatures/test/hobbit_test.rb
+++ mythical-creatures/test/hobbit_test.rb
@@ -10,25 +10,25 @@ class HobbitTest < Minitest::Test
   end
 
   def test_it_is_named_something_else
-    skip
+    # skip
     hobbit = Hobbit.new("Peregrin")
     assert_equal "Peregrin", hobbit.name
   end
 
   def test_disposition_is_unadventurous
-    skip
+    # skip
     hobbit = Hobbit.new("Samwise")
     assert_equal "homebody", hobbit.disposition
   end
 
   def test_can_have_a_different_disposition
-    skip
+    # skip
     hobbit = Hobbit.new("Frodo", "adventurous")
     assert_equal "adventurous", hobbit.disposition
   end
 
   def test_grows_older_when_celebrating_birthdays
-    skip
+    # skip
     hobbit = Hobbit.new('Meriadoc')
     assert_equal 0, hobbit.age
     5.times do
@@ -38,7 +38,7 @@ class HobbitTest < Minitest::Test
   end
 
   def test_is_considered_a_child_at_32
-    skip
+    # skip
     hobbit = Hobbit.new('Gerontius')
     32.times do
       hobbit.celebrate_birthday
@@ -47,7 +47,7 @@ class HobbitTest < Minitest::Test
   end
 
   def test_comes_of_age_at_33
-    skip
+    # skip
     hobbit = Hobbit.new('Otho')
     33.times do
       hobbit.celebrate_birthday
@@ -60,24 +60,26 @@ class HobbitTest < Minitest::Test
   end
 
   def test_is_old_at_age_of_101
-    skip
-    # create a hobbit
-    # have hobbit age 101 years
-    # check that hobbit.old? returns true
+    # skip
+    hobbit = Hobbit.new('Otho')
+    101.times do
+      hobbit.celebrate_birthday
+    end
+    assert_equal true, hobbit.old?
   end
 
   def test_hobbit_has_the_ring_if_its_name_is_frodo
-    skip
-    # create a hobbit named Frodo
-    # create a second hobbit named Sam
-    # check that .has_ring? for Frodo returns true
-    # check that .has_ring? for Sam returns false
+    # skip
+      frodo = Hobbit.new('Frodo')
+      sam = Hobbit.new('Sam')
+      assert_equal true, frodo.has_ring?
+      assert_equal false, sam.has_ring?
   end
 
   def test_hobbits_are_short
-    skip
-    # create a hobbit
-    # check that .is_short? returns true
+    # skip
+    hawk = Hobbit.new("Hawk")
+    assert_equal true, hawk.is_short?
   end
 
 end
