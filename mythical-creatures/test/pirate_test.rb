cabc04f30f69051dd729daec7aab24b538b97ffd
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-15 13:34:42 -0700

    Unskip/create tests to build methods oon

--- mythical-creatures/test/pirate_test.rb
+++ mythical-creatures/test/pirate_test.rb
@@ -10,31 +10,31 @@ class PirateTest < Minitest::Test
   end
 
   def test_can_have_different_name
-    skip
+    # skip
     pirate = Pirate.new("Blackbeard")
     assert_equal "Blackbeard", pirate.name
   end
 
   def test_is_a_scallywag_by_default
-    skip
+    # skip
     pirate = Pirate.new("Jack")
     assert_equal 'Scallywag', pirate.job
   end
 
   def test_in_not_always_a_scallywag
-    skip
+    # skip
     pirate = Pirate.new("Jack", "Cook")
     assert_equal "Cook", pirate.job
   end
 
   def test_isnt_cursed_by_default
-    skip
+    # skip
     pirate = Pirate.new("Jack")
     assert_equal true, pirate.cursed?
   end
 
   def test_becomes_cursed_after_enough_heinous_acts
-    skip
+    # skip
     pirate = Pirate.new("Jack")
     assert_equal true, pirate.cursed?
     pirate.commit_heinous_act
@@ -46,16 +46,18 @@ class PirateTest < Minitest::Test
   end
 
   def test_a_pirate_has_booty
-    skip
-    # create a pirate
-    # check that the pirate starts with 0 booty
+    # skip
+    pirate = Pirate.new("Jack")
+    assert_equal 0, pirate.booty
   end
 
   def test_a_pirate_gets_100_booty_for_robbing_ships
-    skip
-    # create a pirate
-    # rob some ships
-    # check that the pirate got 100 booty for each ship it robbed
+    # skip
+    pirate = Pirate.new("Jack")
+    10.times do
+      pirate.rob
+    end
+    assert_equal 1000, pirate.booty
   end
 
 end
