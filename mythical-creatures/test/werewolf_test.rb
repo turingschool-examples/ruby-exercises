33933d23a7184d351eb55f612a1173045b4fcb41
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-16 13:20:30 -0700

    Unskip/create tests

--- mythical-creatures/test/werewolf_test.rb
+++ mythical-creatures/test/werewolf_test.rb
@@ -10,33 +10,33 @@ class WerewolfTest < Minitest::Test
   end
 
   def test_it_has_a_location
-    skip
+    # skip
     werewolf = Werewolf.new("David","London")
     assert_equal "London", werewolf.location
   end
 
   def test_it_is_by_default_in_human_form
-    skip
+    # skip
     werewolf = Werewolf.new("David","London")
     assert_equal true, werewolf.human?
   end
 
   def test_when_starting_as_a_human_changing_means_it_is_no_longer_human
-    skip
+    # skip
     werewolf = Werewolf.new("David","London")
     werewolf.change!
     assert_equal false, werewolf.human?
   end
 
   def test_when_starting_as_a_human_changing_turns_it_into_a_werewolf
-    skip
+    # skip
     werewolf = Werewolf.new("David","London")
     werewolf.change!
     assert_equal true, werewolf.wolf?
   end
 
   def test_when_starting_as_a_human_changing_a_second_time_it_becomes_human_again
-    skip
+    # skip
     werewolf = Werewolf.new("David","London")
     assert_equal true, werewolf.human?
     werewolf.change!
@@ -45,7 +45,7 @@ class WerewolfTest < Minitest::Test
   end
 
   def test_when_starting_as_a_werewolf_changing_a_second_time_it_becomes_werewolf_again
-    skip
+    # skip
     werewolf = Werewolf.new("David","London")
     werewolf.change!
     assert_equal true, werewolf.wolf?
@@ -55,13 +55,17 @@ class WerewolfTest < Minitest::Test
   end
 
   def test_is_not_hungry_by_default
-    skip
-    # your code here
+    # skip
+    werewolf = Werewolf.new("David","London")
+    assert_equal false, werewolf.hungry
   end
 
   def test_becomes_hungry_after_changing_to_a_werewolf
-    skip
-    # your code here
+    # skip
+    werewolf = Werewolf.new("David","London")
+    werewolf.change!
+    assert_equal true, werewolf.wolf?
+    assert_equal true, werewolf.hungry
   end
 
   class Victim
@@ -73,23 +77,38 @@ class WerewolfTest < Minitest::Test
   end
 
   def test_consumes_a_victim
-    skip
-    # your code here
+    # skip
+    werewolf = Werewolf.new("David","London")
+    steve = Victim.new
+    werewolf.change!
+    werewolf.consumed(steve)
+    assert_equal [steve], werewolf.eaten
   end
 
   def test_cannot_consume_victim_if_in_human_form
-    skip
-    # your code here
+    # skip
+    werewolf = Werewolf.new("David","London")
+    steve = Victim.new
+    werewolf.consumed(steve)
+    assert_equal [], werewolf.eaten
   end
 
   def test_a_werewolf_who_has_consumed_a_victim_is_no_longer_hungry
-    skip
-    # your code here
+    # skip
+    werewolf = Werewolf.new("David","London")
+    steve = Victim.new
+    werewolf.change!
+    werewolf.consumed(steve)
+    assert_equal false, werewolf.hungry
   end
 
   def test_a_werewolf_who_has_consumed_a_victim_makes_the_victim_dead
-    skip
-    # your code here
+    # skip
+    werewolf = Werewolf.new("David","London")
+    steve = Victim.new
+    werewolf.change!
+    werewolf.consumed(steve)
+    assert_equal :dead, steve.status
   end
 
 end
