14bd43f24291da54651c21f99fdac8510d3f0ad3
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-16 13:20:51 -0700

    Build methods to pass tests

--- mythical-creatures/lib/werewolf.rb
+++ mythical-creatures/lib/werewolf.rb
@@ -1,1 +1,47 @@
-class Were
+class Werewolf
+  attr_reader  :name,
+                    :location,
+                    :human,
+                    :change,
+                    :hungry,
+                    :wolf,
+                    :eaten
+
+  def initialize(name, location = "London")
+    @name = name
+    @location = location
+    @human = true
+    @change = 0
+    @wolf = false
+    @hungry = false
+    @eaten = []
+  end
+
+  def human?
+    @human
+  end
+
+  def change!
+    @change += 1
+      if @change.odd? == true
+       @human = false
+     elsif @change.odd? == false
+       @human = true
+    end
+  end
+
+  def wolf?
+    if @human == false
+      @wolf = true
+      @hungry = true
+    end
+  end
+
+  def consumed(victim)
+    if @human == false
+      @eaten << victim
+      @hungry = false
+      victim.status = :dead
+    end
+  end
+end
