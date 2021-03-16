36dd1448ae9afc7b103223fb106f3afb97aa7c4b
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-03-15 21:31:49 -0700

    Unskip/create tests

--- mythical-creatures/test/medusa_test.rb
+++ mythical-creatures/test/medusa_test.rb
@@ -10,13 +10,13 @@ class MedusaTest < Minitest::Test
   end
 
   def test_when_first_created_she_has_no_statues
-    skip
+    # skip
     medusa = Medusa.new("Cassiopeia")
     assert_equal true, medusa.statues.empty?
   end
 
   def test_when_staring_at_a_person_she_gains_a_statue
-    skip
+    # skip
     medusa = Medusa.new("Cassiopeia")
     victim = Person.new("Perseus")
 
@@ -26,7 +26,7 @@ class MedusaTest < Minitest::Test
   end
 
   def test_when_staring_at_a_person_that_person_turns_to_stone
-    skip
+    # skip
     medusa = Medusa.new("Cassiopeia")
     victim = Person.new("Perseus")
 
@@ -36,13 +36,35 @@ class MedusaTest < Minitest::Test
   end
 
   def test_can_only_have_three_victims
-    skip
-    # your code here
+    # skip
+    medusa = Medusa.new("Cassiopeia")
+    persues = Person.new("Perseus")
+    hawk = Person.new("Hawk")
+    dalia = Person.new("Dalia")
+    ted = Person.new("Ted")
+    medusa.stare(persues)
+    medusa.stare(hawk)
+    medusa.stare(dalia)
+    medusa.stare(ted)
+    assert_equal 3, medusa.statues.length
+
+
   end
 
   def test_if_a_fourth_victim_is_stoned_first_is_unstoned
-    skip
-    # your code here
+    # skip
+    medusa = Medusa.new("Cassiopeia")
+    persues = Person.new("Perseus")
+    hawk = Person.new("Hawk")
+    dalia = Person.new("Dalia")
+    ted = Person.new("Ted")
+    medusa.stare(persues)
+    medusa.stare(hawk)
+    medusa.stare(dalia)
+    medusa.stare(ted)
+    assert_equal [hawk, dalia, ted], medusa.statues
+    assert_equal false, persues.stoned?
+    assert_equal true, ted.stoned?
   end
 
 end
