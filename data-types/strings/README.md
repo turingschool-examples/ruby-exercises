1f5923d5d6a1f7aca08e1a3f0e9be3da1941bc01
Michael Dao <mike@turing.io>
2021-03-22 07:59:15 -0600

    strings

--- data-types/strings/README.md
+++ data-types/strings/README.md
@@ -1,4 +1,4 @@
-The `strings.rb` file has a number of exercises to help you better work with strings in ruby.  
+The `strings.rb` file has a number of exercises to help you better work with strings in ruby.
 
 Below is a list of methods that will help you complete the exercises - research each of them on the [Ruby Docs for Strings](https://ruby-doc.org/core-2.6.5/String.html) to learn more about what they do and how they are used.
 
@@ -19,64 +19,5 @@ There are additional methods that you will need to complete the exercises; each 
 * `String#upcase`[docs](https://ruby-doc.org/core-2.6.5/String.html#method-i-upcase)
 
 
-[Here's a walk-through to this exercise](https://youtu.be/aeAkLxr5diE). It touches on far more than just the exact solutions to given answers, so please give it a watch, or at least watch a few chunks of it:
 
-[![Intro to ruby-exercises and strings.rb (YouTube)](/images/embedded-video-screenshot-getting-started.jpg)](https://youtu.be/aeAkLxr5diE)
-
-#### What is minitest?
-
-You'll be using a tool here called `Minitest`. Here's great additional reading:
-
-[Assert Yourself: An Introduction to Minitest](https://launchschool.com/blog/assert-yourself-an-introduction-to-minitest)
-
-
-----------------
-
-
-Did you read the Launch School post? Great! Then most of what is in this file should look a lot more readable to you.
-
-As a quick test, answer the following questions:
-
-- What is minitest?
-- What does `assert_equal` mean? How many arguments does it expect?
-- What will `assert_equal true, true` return?
-- How do you “run” a test file?
-- is a “failing” test a bad thing?
-- does minitest run all the tests in order?
-- Find a list of minitest assertions (google it!). What’s a cool one you found?
-
-Awesome. If you can answer all of those questions, you’ve got your head wrapped around Minitest, at least enough to start these exercises. See the walk-through above to get started, or jump to a specific spot of the video below:
-
-```
-Timestamps:
-0:40 - run `git clone` to copy down the repository
-1:04 - `bundle` command throws a big error! "Could not find 'bundler'..."
-1:30 - navigating to the correct directory to start the first recommended exercise
-1:48 - opening up the correct directory in Atom
-1:50 - using "tab complete" in the terminal
-2:22 - "preview" the readme, so it's more easily readable in your editor
-2:28 - first use of "command pallet"
-2:56 - running the tests for the first time! Success!
-3:09 - starting test_1, first getting an error, which is actually a sign of success
-4:26 - ready to make test_1 pass, reading documentation, understanding details about ruby code, more keyboard shortcuts
-8:09 - test_2
-10:50 test_3
-12:08 test_4
-13:13 test_5
-16:12 test_6
-17:37 test_7
-19:47 test_8
-21:12 test_9
-23:29 test_10
-24:20 test_11
-25:19 test_12
-28:40 test_13
-29:18 test_14
-29:33 test_15
-29:48 test_16
-31:19 TAKE A BREAK PLEASE!
-31:32 test_17
-32:50 test_18
-35:17 test_19
-35:35 test_20, 21, 22, 23, 24 - combined into one chunk at 23:35
 ```
--- data-types/strings/spec/strings_spec.rb
+++ data-types/strings/spec/strings_spec.rb
@@ -1,251 +1,212 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-
-# There's a video walk-through of this exercise available here:
-# https://youtu.be/aeAkLxr5diE
-# and a helpful blog post on this general topic here:
-# https://josh.works/turing-backend-prep-02-first-tests-and-making-them-pass
-
-class StringTest < Minitest::Test
-  def test_1
-    skip
+RSpec.describe 'Strings' do
+  it 'test 1' do
     name = "alice"
     # In place of the line below, call a method on the name variable
     # defined above to acheive the expected output.
     actual = name._____
     expected = "Alice"
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_2
-    skip
+  xit 'test 2' do
     name = "aLiCe"
     # In place of the line below, call a method to achieve the expected output.
     actual = name._____
     expected = "ALICE"
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_3
-    skip
+  xit 'test 3' do
     name = "AlIcE"
     # In place of the line below, call a method to achieve the expected output.
     actual = name._____
     expected = "alice"
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_4
-    skip
+  xit 'test 4' do
     rhyme = "peter piper picked a peck of picked peppers"
     # In place of the line below, call a method to achieve the expected output.
     actual = rhyme._____
     expected = "sreppep dekcip fo kcep a dekcip repip retep"
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_5
-    skip
+  xit 'test 5' do
     word = "ticking"
     # In place of the line below, call a method to achieve the expected output.
     actual = word.______
     expected = "kicking"
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_6
-    skip
+  xit 'test 6' do
     word = "ticking"
     # In place of the line below, call a method to achieve the expected output.
     actual = word.______
     expected = "clocking"
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_7
-    skip
+  xit 'test 7' do
     words = "five sleepy kittens"
     # In place of the line below, call a method to achieve the expected output.
     actual = words.______
     expected = "fiv* sl**py kitt*ns"
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_8
-    skip
+  xit 'test 8' do
     greeting = "Hello!!"
     # In place of the line below, call a method to achieve the expected output.
     actual = greeting._____
     expected = "Hello!"
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_9
-    skip
+  xit 'test 9' do
     greeting = "Hello!!\n"
     # In place of the line below, call a method to achieve the expected output.
     actual = greeting._____
     expected = "Hello!!"
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_10
-    skip
+  xit 'test 10' do
     greeting = "Hello!!\n\n"
     # In place of the line below, call a method to achieve the expected output.
     actual = greeting._____
     expected = "Hello!!\n"
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_11
-    skip
+  xit 'test 11' do
     rhyme = "eeny, meeny, miny, moe"
     # In place of the line below, call a method to achieve the expected output.
     actual = rhyme._____
     expected = "ny, mny, miny, mo"
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_12
-    skip
+  xit 'test 12' do
     rhyme = "eeny, meeny, miny, moe"
     # In place of the line below, call a method to achieve the expected output.
     actual = rhyme._____
     expected = "ny, mny, mny, m"
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_13
-    skip
+  xit 'test 13' do
     greeting = "Hello World!"
     # In place of the line below, call a method to get the number of characters in the string
     actual = greeting._____
     expected = 12
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_14
-    skip
+  xit 'test 14' do
     greeting = "Hello World!\n"
     # In place of the line below, call a method to get the number of characters in the string
     actual = greeting._____
     expected = 13
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_15
-    skip
+  xit 'test 15' do
     greeting = "Hello       World!"
     # In place of the line below, call a method to get the number of characters in the string
     actual = greeting._____
     expected = 18
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_16
-    skip
+  xit 'test 16' do
     greeting = "Hello World!"
     # In place of the line below, call a method to get the number of 'o' in the string
     actual = greeting._____
     expected = 2
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_17
-    skip
+  xit 'test 17' do
     greeting = "Hello World!"
     # In place of the line below, call a method to get the number of vowels in the string
     actual = greeting._____
     expected = 3
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_18
-    skip
+  xit 'test 18' do
     greeting = "Hello World!"
     # In place of the line below, call a method to check if the string includes 'llo'
     actual = greeting._____
     expected = true
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_19
-    skip
+  xit 'test 19' do
     greeting = "Hello World!"
     # In place of the line below, call a method to check if the string includes 'lol'
     actual = greeting._____
     expected = false
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_20
-    skip
+  xit 'test 20' do
     greeting = "Hello World, my name is"
     name = "Harry Potter"
     # In place of the line below, use string manipulation to combine the
     #greeting and name variables to acheive the expected outcome
     actual = _________
     expected = "Hello World, my name is Harry Potter"
-    
-    assert_equal expected, actual
+
+    expect(actual).to eq(expected)
   end
-  
-  def test_21
-    skip
+
+  xit 'test 21' do
     # See if you can use another method than the last test to achieve the same goal:
     greeting = "Hello World, my name is"
     name = "Harry Potter"
     actual = ________
     expected = "Hello World, my name is Harry Potter"
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
-  
-  def test_22
-    skip
-    # Again, using a different method:
+
+  xit 'test 22' do
+    greeting = "Hello World, my name is"
+    name = "Harry Potter"
     actual = ________
     expected = "Hello World, my name is Harry Potter"
 
-    assert_equal expected, actual
-  end
-  
-  def test_23
-    skip
-    # Once more, using a different method:
-    actual = ________
-    expected = "Hello World, my name is Harry Potter"
-
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 
-  def test_24
-    skip
+  xit 'test 23' do
     phrase = "  \n\t to the moon\n\n\t    "
     # In place of the line below, call a method to acheive the expected outcome
     actual = ________
     expected = "to the moon"
 
-    assert_equal expected, actual
+    expect(actual).to eq(expected)
   end
 end
