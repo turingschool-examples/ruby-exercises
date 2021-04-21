fae84e6d5cd927b8abe53a7d72f8d8de71dbf186
Mayowa Ojo <mayowaojo.e@gmail.com>
2021-04-21 15:23:35 -0600

    fix variable name error

--- initialize/spec/dog_spec.rb
+++ initialize/spec/dog_spec.rb
@@ -5,18 +5,18 @@ RSpec.describe Dog do
   it 'exists' do
     doug = Dog.new("Doug", "Golden Retriever")
 
-    expect(dog).to be_an_instance_of(Dog)
+    expect(doug).to be_an_instance_of(Dog)
   end
 
   it 'has a greeting' do
     doug = Dog.new("Doug", "Golden Retriever")
 
-    expect(dog.greeting).to eq("Woof, I'm Doug the Golden Retriever!")
+    expect(doug.greeting).to eq("Woof, I'm Doug the Golden Retriever!")
   end
 
   it 'can have a different greeting' do
     dolly = Dog.new("Dolly", "Lab")
 
-    expect(dog.greeting).to eq("Woof, I'm Dolly the Lab!")
+    expect(dolly.greeting).to eq("Woof, I'm Dolly the Lab!")
   end
 end
