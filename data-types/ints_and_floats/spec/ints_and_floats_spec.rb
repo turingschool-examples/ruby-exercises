ee47c465794bbafcf50cbad8925f1582a46491a7
Saundra Catalina <saundracatalina@gmail.com>
2021-05-01 16:08:42 -0600

    Add passing tests 6-9. All tests passing

--- data-types/ints_and_floats/spec/ints_and_floats_spec.rb
+++ data-types/ints_and_floats/spec/ints_and_floats_spec.rb
@@ -41,39 +41,39 @@ RSpec.describe 'ints and floats' do
     unlucky = 13
     # Using the two variables defined above,
     # find the remainder of the unlucky divided by the lucky
-    remainder = unlucky % lucky 
+    remainder = unlucky % lucky
     expect(remainder).to eq(6)
   end
 
-  xit 'test 6' do
+  it 'test 6' do
     lucky = 7
     # Using the variable defined above,
     # find out if the lucky number is even
-    even = _________
+    even = lucky.even?
     expect(even).to eq(false)
   end
 
-  xit 'test 7' do
+  it 'test 7' do
     pi = 3.14
     # Using the variable defined above,
     # round the number to the nearest whole number
-    rounded = _________
+    rounded = pi.round
     expect(rounded).to eq(3)
   end
 
-  xit 'test 8' do
+  it 'test 8' do
     pi = 3.14
     # Using the variable defined above,
     # round the number to one decimal place
-    rounded = _________
+    rounded = pi.round(1)
     expect(rounded).to eq(3.1)
   end
 
-  xit 'test 9' do
+  it 'test 9' do
     pi = 3.14
     # Using the variable defined above,
     # round the number to the next highest whole number
-    rounded = _________
+    rounded = pi.ceil
     expect(rounded).to eq(4)
   end
 end
