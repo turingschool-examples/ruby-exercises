bb3e536b5d2b5ddf829e8a0c9fb1e0577c6a7f5d
Richard Tillies <rtillies@gmail.com>
2022-03-31 09:52:03 -0400

    has a different kind of candy

--- objects-and-methods/exercise-1/spec/candy_spec.rb
+++ objects-and-methods/exercise-1/spec/candy_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Candy do
     expect(candy.type).to eq('Skittles')
   end
 
-  xit 'has a different kind of candy' do
+  it 'has a different kind of candy' do
     candy = Candy.new('Snickers')
 
     expect(candy.type).to eq('Snickers')
