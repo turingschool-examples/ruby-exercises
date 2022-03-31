6a44d639bc756d42205e65ae6fe941114e0d5317
Richard Tillies <rtillies@gmail.com>
2022-03-31 16:36:05 -0400

    test: Refactor << to #add_candy method

--- objects-and-methods/exercise-1/spec/bag_spec.rb
+++ objects-and-methods/exercise-1/spec/bag_spec.rb
@@ -5,7 +5,7 @@ require_relative '../lib/candy'
 RSpec.describe Bag do
   it 'is empty' do
     bag = Bag.new
-    
+
     expect(bag.empty?).to be true
   end
 
@@ -24,28 +24,28 @@ RSpec.describe Bag do
   xit 'can put a candy in a bag' do
     bag = Bag.new
     candy = Candy.new('Sour frogs')
-    bag << candy
+    bag.add_candy candy
 
     expect(bag.candies).to eq([candy])
   end
 
   xit 'is not empty when it has candies' do
     bag = Bag.new
-    bag << Candy.new("Nerds")
+    bag.add_candy Candy.new("Nerds")
 
     expect(bag.empty?).to be false
   end
 
   xit 'can count candies' do
     bag = Bag.new
-    bag << Candy.new("Caramelized Almonds")
+    bag.add_candy Candy.new("Caramelized Almonds")
 
     expect(bag.count).to eq(1)
   end
 
   xit 'contains candies and candies have a type' do
     bag = Bag.new
-    bag << Candy.new("Hershey's Kisses")
+    bag.add_candy Candy.new("Hershey's Kisses")
     # You usually don't want to chain a bunch of different
     # types of things together like this.
     # We'll talk about it more in a few weeks.
@@ -57,7 +57,7 @@ RSpec.describe Bag do
 
   xit 'can be asked if it has a particular kind of candy' do
     bag = Bag.new
-    bag << Candy.new("Lindt chocolate")
+    bag.add_candy Candy.new("Lindt chocolate")
 
     expect(bag.contains?('Lindt chocolate')).to be true
     expect(bag.contains?('Nerds')).to be false
