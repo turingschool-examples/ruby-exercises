353435aa9e426fb31b577f9eb2a56d46bd289df5
Melony Franchini <meltravelz@gmail.com>
2022-12-21 16:35:12 -0700

    Add data-types, integers

--- data-types/ints_and_floats/spec/ints_and_floats_spec.rb
+++ data-types/ints_and_floats/spec/ints_and_floats_spec.rb
@@ -4,76 +4,77 @@ RSpec.describe 'ints and floats' do
     unlucky = 13
     # Using the two variables defined above,
     # add the lucky number and the unlucky number
-    sum = ________
-    expect(sum).to eq(20)
+    # sum = ________
+    expect(lucky + unlucky).to eq(20)
   end
 
-  xit 'test 2' do
+  it 'test 2' do
     lucky = 7
     unlucky = 13
     # Using the two variables defined above,
     # subtract the unlucky from the lucky
-    difference = ________
-    expect(difference).to eq(-6)
+    # difference = ________
+    expect(lucky - unlucky).to eq(-6)
   end
 
-  xit 'test 3' do
+  it 'test 3' do
     lucky = 7
     unlucky = 13
     # Using the two variables defined above,
     # divide unlucky by lucky
     # NOTE: this is integer division
-    quotient = ________
-    expect(quotient).to eq(1)
+    # quotient = ________
+    expect(unlucky / lucky).to eq(1)
+    expect(unlucky.div(lucky)).to eq(1)
   end
 
-  xit 'test 4' do
+  it 'test 4' do
     lucky = 7
     unlucky = 13
     # Using the two variables defined above,
     # divide unlucky by lucky
-    quotient = ________
-    expect(quotient).to eq(1.8571428571428572)
+    # quotient = ________
+    expect(unlucky.fdiv(lucky)).to eq(1.8571428571428572)
   end
 
-  xit 'test 5' do
+  it 'test 5' do
     lucky = 7
     unlucky = 13
     # Using the two variables defined above,
     # find the remainder of the unlucky divided by the lucky
-    remainder = ____________
-    expect(remainder).to eq(6)
+    # remainder = ____________
+    expect(unlucky.remainder(lucky)).to eq(6)
   end
 
-  xit 'test 6' do
+  it 'test 6' do
     lucky = 7
     # Using the variable defined above,
     # find out if the lucky number is even
-    even = _________
-    expect(even).to eq(false)
+    # even = _________
+    expect(lucky.even?).to eq(false)
   end
 
-  xit 'test 7' do
+  it 'test 7' do
     pi = 3.14
     # Using the variable defined above,
     # round the number to the nearest whole number
-    rounded = _________
-    expect(rounded).to eq(3)
+    # rounded = _________
+    expect(pi.round).to eq(3)
   end
 
-  xit 'test 8' do
+  it 'test 8' do
     pi = 3.14
     # Using the variable defined above,
     # round the number to one decimal place
-    rounded = _________
-    expect(rounded).to eq(3.1)
+    # rounded = _________
+    expect(pi.round(1)).to eq(3.1)
   end
 
-  xit 'test 9' do
+  it 'test 9' do
     pi = 3.14
     # Using the variable defined above,
     # round the number to the next highest whole number
-    rounded = _________
-    expect(rounded).to eq(4)
+    # rounded = _________
+    expect(pi.ceil).to eq(4)
   end
 end
