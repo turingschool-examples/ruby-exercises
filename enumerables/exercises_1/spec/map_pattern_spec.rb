310dac6a7fa047c989821e82d85609d20e562d07
Melony Franchini <meltravelz@gmail.com>
2022-12-22 17:41:16 -0700

    Add enumerables map

--- enumerables/exercises_1/spec/map_pattern_spec.rb
+++ enumerables/exercises_1/spec/map_pattern_spec.rb
@@ -3,55 +3,88 @@ RSpec.describe 'map pattern' do
   it 'capitalizes' do
     names = ["alice", "bob", "charlie"]
     capitalized_names = []
+    
     names.each do |name|
       capitalized_names << name.capitalize
     end
+
     expect(capitalized_names).to eq(["Alice", "Bob", "Charlie"])
   end
 
-  xit 'doubles' do
+  it 'doubles' do
     numbers = [1, 2, 3, 4, 5]
     doubles = []
+
     numbers.each do |number|
-      # Your code goes here
+      doubles << number * 2
     end
+
     expect(doubles).to eq([2, 4, 6, 8, 10])
   end
 
-  xit 'squares' do
+  it 'squares' do
     numbers = [1, 2, 3, 4, 5]
     squares = []
-    # Your code goes here
+
+    numbers.each do |num|
+      squares << num * num
+    end
+
     expect(squares).to eq([1, 4, 9, 16, 25])
   end
 
-  xit 'lengths' do
+  it 'lengths' do
     names = ["alice", "bob", "charlie", "david", "eve"]
-    # Your code goes here
+    lengths = []
+
+    names.each do |name|
+      lengths << name.length
+    end
+
     expect(lengths).to eq([5, 3, 7, 5, 3])
   end
 
-  xit 'normalize zip codes' do
+  it 'normalize zip codes' do
     numbers = [234, 10, 9119, 38881]
-    # Your code goes here
+    zip_code = []
+
+    numbers.each do |num|
+      zip_code << num.to_s.rjust(5, "0")
+    end
+
     expect(zip_code).to eq(["00234", "00010", "09119", "38881"])
   end
 
-  xit 'backwards' do
+  it 'backwards' do
     names = ["alice", "bob", "charlie", "david", "eve"]
-    # Your code goes here
+    backwards = []
+
+    names.each do |name|
+      backwards << name.reverse
+    end
+
     expect(backwards).to eq(["ecila", "bob", "eilrahc", "divad", "eve"])
   end
 
-  xit 'words with no vowels' do
+  it 'words with no vowels' do
     words = ["green", "sheep", "travel", "least", "boat"]
-    # Your code goes here
+    without_vowels = []
+
+    words.each do |word|
+      without_vowels.push(word.delete("aeiou"))
+    end
+
     expect(without_vowels).to  eq(["grn", "shp", "trvl", "lst", "bt"])
   end
 
-  xit 'trims last letter' do
+  it 'trims last letter' do
     animals = ["dog", "cat", "mouse", "frog", "platypus"]
-    # Your code goes here
+    trimmed = []
+
+    animals.each do |animal|
+      trimmed << animal.chop
+    end
+
     expect(trimmed).to eq(["do", "ca", "mous", "fro", "platypu"])
   end
 end
--- enumerables/exercises_1/spec/map_spec.rb
+++ enumerables/exercises_1/spec/map_spec.rb
@@ -2,53 +2,68 @@ RSpec.describe 'map' do
 
   it 'capitalizes' do
     names = ["alice", "bob", "charlie"]
+    
     capitalized_names = names.map do |name|
       name.capitalize
     end
+    # <.map> doesn't need an accumulator cuz you can define a variable that will become the modified array
+    # <.each> needs one because it returns the original array!
     expect(capitalized_names).to eq(["Alice", "Bob", "Charlie"])
   end
 
-  xit 'doubles' do
+  it 'doubles' do
     numbers = [1, 2, 3, 4, 5]
     doubles = numbers.map do |number|
-      # Your code goes here
+      number * 2
     end
     expect(doubles).to eq([2, 4, 6, 8, 10])
   end
 
-  xit 'squares' do
+  it 'squares' do
     numbers = [1, 2, 3, 4, 5]
-    # Your code goes here
+    squares = numbers.map do |num|
+      num * num
+    end
     expect(squares).to eq([1, 4, 9, 16, 25])
   end
 
-  xit 'lengths' do
+  it 'lengths' do
     names = ["alice", "bob", "charlie", "david", "eve"]
-    # Your code goes here
+    lengths = names.map do |name|
+      name.length
+    end
     expect(lengths).to eq([5, 3, 7, 5, 3])
   end
 
-  xit 'normalize zip codes' do
+  it 'normalize zip codes' do
     numbers = [234, 10, 9119, 38881]
-    # Your code goes here
+    zip_codes = numbers.map do |num|
+      num.to_s.rjust(5, "0")
+    end
     expect(zip_codes).to eq(["00234", "00010", "09119", "38881"])
   end
 
-  xit 'backwards' do
+  it 'backwards' do
     names = ["alice", "bob", "charlie", "david", "eve"]
-    # Your code goes here
+    backwards = names.map do |name|
+      name.reverse
+    end
     expect(backwards).to eq(["ecila", "bob", "eilrahc", "divad", "eve"])
   end
 
-  xit 'words with no vowels' do
+  it 'words with no vowels' do
     words = ["green", "sheep", "travel", "least", "boat"]
-    # Your code goes here
+    without_vowels = words.map do |word|
+      word.delete("aeiou")
+    end
     expect(without_vowels).to eq(["grn", "shp", "trvl", "lst", "bt"])
   end
 
-  xit 'trims last letter' do
+  it 'trims last letter' do
     animals = ["dog", "cat", "mouse", "frog", "platypus"]
-    # Your code goes here
+    trimmed = animals.map do |animal|
+      animal.chop
+    end
     expect(trimmed).to eq(["do", "ca", "mous", "fro", "platypu"])
   end
 end
