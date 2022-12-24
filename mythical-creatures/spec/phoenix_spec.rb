fe7368202433fc70feae5af36b4fefecd7ce8a68
Melony Franchini <meltravelz@gmail.com>
2022-12-23 18:04:07 -0700

    Add more phoenix test ideas

--- mythical-creatures/spec/phoenix_spec.rb
+++ mythical-creatures/spec/phoenix_spec.rb
@@ -16,6 +16,14 @@ RSpec.describe Phoenix do
 		expect(phoenix.mood).to eq("stoic")
 	end
 
+	xit "can age" do
+	end
+
+	xit "can will burst into flames every 500 years" do
+	end
+
+	xit "is reborn after bursting into flames" do
+	end
 
 	it "can change color & mood when feeling an emotion" do
 		phoenix = Phoenix.new("Bennu")
@@ -36,7 +44,7 @@ RSpec.describe Phoenix do
 		expect(phoenix.emotional_tracker[:joyful]).to eq(3)
 	end
 
-	it "releases a single tear after feeling the same emotion 3 times" do
+	it "releases a tear after feeling the same emotion 3 times & returns to default mood & color" do
 		phoenix = Phoenix.new("Bennu")
 
 		phoenix.feels_emotion(:joyful)
@@ -45,25 +53,31 @@ RSpec.describe Phoenix do
 
 		expect(phoenix.emotional_tracker[:joyful]).to eq(3)
 		expect(phoenix.releases_tear?).to be true
+		expect(phoenix.mood).to eq("stoic")
+		expect(phoenix.color).to eq("golden")
 	end
 
-	it "it forgets that emotion after feeling it 3 times" do
+	it "it forgets an emotion after releasing a tear/feeling the same emotion 3 times" do
 		phoenix = Phoenix.new("Bennu")
 
 		phoenix.feels_emotion(:joyful)
-		expect(phoenix.emotional_tracker[:joyful]).to eq(1)
-
 		phoenix.feels_emotion(:joyful)
-		expect(phoenix.emotional_tracker[:joyful]).to eq(2)
-
-		phoenix.feels_emotion(:joyful)
+		phoenix.feels_emotion(:joyful)	
 		expect(phoenix.emotional_tracker[:joyful]).to eq(3)
-		expect(phoenix.releases_tear?).to be true
 
 		phoenix.feels_emotion(:joyful)
 		expect(phoenix.emotional_tracker[:joyful]).to eq(1)
 		expect(phoenix.releases_tear?).to be false
 	end
 
+	xit "feels joyful when being near a human" do
+	end
+
+
+	xit "when that human is sick it's tear can heal the human" do
+	end
+
+	xit "spends time with humans" do
+	end
 
 end
