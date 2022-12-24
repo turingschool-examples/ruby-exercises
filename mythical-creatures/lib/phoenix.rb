e304ba0d0c4bcfb4698778b7280482a3f1e69990
Melony Franchini <meltravelz@gmail.com>
2022-12-24 11:44:19 -0700

    Add phoenix test/methods but stuck now

--- mythical-creatures/lib/phoenix.rb
+++ mythical-creatures/lib/phoenix.rb
@@ -1,40 +1,91 @@
 class Phoenix
-		attr_reader :name,
-								:color,
-								:mood,
-								:emotional_tracker
+	attr_reader :name,
+							:color,
+							:mood,
+							:emotional_awareness
+
+	attr_accessor :feels_emotion(emotion)
 
 	def initialize(name, color = "golden", mood = "stoic")
 		@name = name
 		@color = color
 		@mood = mood
-		@emotional_tracker = {}
+		@emotional_awareness = {}
 		@releases_tear = false
 	end
 
-	def feels_emotion(emotion)
-		@color = "sarlet"
-		@mood = "heated"
+	def burst_into_flame
+			@color = "golden"
+			@mood = "stoic"
+			@emotional_awareness = {}
+	end
 
-		if @emotional_tracker.has_key?(emotion)
-			@emotional_tracker[emotion] += 1
+	def feels_emotion(emotion)
+		if @emotional_awareness.has_key?(emotion)
+			@emotional_awareness[emotion] += 1
 		
-			if @emotional_tracker[emotion] == 3
+			if @emotional_awareness[emotion] == 2
+				@color = "scarlet"
+				@mood = "fiery"
+			elsif @emotional_awareness[emotion] == 3
 				@releases_tear = true
-				@color = "golden"
-				@mood = "stoic"
-			elsif @emotional_tracker[emotion] > 3
+				@color = "crimson"
+				@mood = "ablaze"
+			elsif @emotional_awareness[emotion] == 4
 				@releases_tear = false
-				@emotional_tracker[emotion] = 1
+				@color = "deep violet"
+				@mood = "incandescent"
+			elsif @emotional_awareness[emotion] >= 5
+				burst_into_flame
 			end
-			
+
 		else
-			@emotional_tracker[emotion] = 1
+			@emotional_awareness[emotion] = 1
+			@color = "amber"
+			@mood = "heated"
 		end
 	end
 
 	def releases_tear?
 		@releases_tear
+	end
+end
+
+
+class Pharaoh
+	attr_reader :name,
+				:reputation,
+				:dynastic_period
+
+	def initialize(name, reputation, dynastic_period)
+		@name = name
+		@reputation = reputation
+		@dynastic_period = dynastic_period
+		@health = true
+		@alive = true
+	end
+
+	def healthy?
+		@health
+	end
+
+	def age(number)
+		if number >= 30
+			@health = false
+		end
+	end
+
+	def takes_action(emotion)
+		phoenix.feels_emotion(emotion)
+	end
+
+	def alive?
+		@alive
+	end
+
+	def dies
+		@alive == false
+		5.times { phoenix.feels_emotion(:sorrow) }
 	end
 
 end
--- mythical-creatures/spec/phoenix_spec.rb
+++ mythical-creatures/spec/phoenix_spec.rb
@@ -5,79 +5,146 @@ RSpec.describe Phoenix do
 
 	it "exists" do
 		phoenix = Phoenix.new("Bennu")
-
 		expect(phoenix.name).to eq("Bennu")
 	end
 
 	it "is born a golden color and in a stoic mood" do
 		phoenix = Phoenix.new("Bennu")
-
 		expect(phoenix.color).to eq("golden")
 		expect(phoenix.mood).to eq("stoic")
 	end
 
-	xit "can age" do
-	end
-
-	xit "can will burst into flames every 500 years" do
-	end
-
-	xit "is reborn after bursting into flames" do
-	end
-
-	it "can change color & mood when feeling an emotion" do
+	it "changes color & mood when feeling an emotion" do
 		phoenix = Phoenix.new("Bennu")
-
 		phoenix.feels_emotion(:joyful)
-
-		expect(phoenix.color).to eq("sarlet")
+		expect(phoenix.color).to eq("amber")
 		expect(phoenix.mood).to eq("heated")
 	end
 
-	it "can track how many times it has the same emotion" do
+	it "has emotional awareness about how many times it has the same emotion" do
+		phoenix = Phoenix.new("Bennu")
+		phoenix.feels_emotion(:joyful)
+		expect(phoenix.emotional_awareness[:joyful]).to eq(1)
+		phoenix.feels_emotion(:joyful)
+		expect(phoenix.emotional_awareness[:joyful]).to eq(2)
+	end 
+
+	it "changes color & mood when feeling an emotion 2, 3, and 4 times" do
 		phoenix = Phoenix.new("Bennu")
 
-		phoenix.feels_emotion(:joyful)
-		phoenix.feels_emotion(:joyful)
-		phoenix.feels_emotion(:joyful)
+		2.times { phoenix.feels_emotion(:joyful) }
+		expect(phoenix.color).to eq("scarlet")
+		expect(phoenix.mood).to eq("fiery")
 
-		expect(phoenix.emotional_tracker[:joyful]).to eq(3)
+		phoenix.feels_emotion(:joyful)
+		expect(phoenix.color).to eq("crimson")
+		expect(phoenix.mood).to eq("ablaze")
+
+		phoenix.feels_emotion(:joyful)
+		expect(phoenix.color).to eq("deep violet")
+		expect(phoenix.mood).to eq("incandescent")
 	end
 
-	it "releases a tear after feeling the same emotion 3 times & returns to default mood & color" do
+	it "only releases a tear after feeling the same emotion 3 times" do
 		phoenix = Phoenix.new("Bennu")
-
-		phoenix.feels_emotion(:joyful)
-		phoenix.feels_emotion(:joyful)
-		phoenix.feels_emotion(:joyful)
-
-		expect(phoenix.emotional_tracker[:joyful]).to eq(3)
+		3.times { phoenix.feels_emotion(:joyful) }
 		expect(phoenix.releases_tear?).to be true
-		expect(phoenix.mood).to eq("stoic")
-		expect(phoenix.color).to eq("golden")
-	end
-
-	it "it forgets an emotion after releasing a tear/feeling the same emotion 3 times" do
-		phoenix = Phoenix.new("Bennu")
-
 		phoenix.feels_emotion(:joyful)
-		phoenix.feels_emotion(:joyful)
-		phoenix.feels_emotion(:joyful)	
-		expect(phoenix.emotional_tracker[:joyful]).to eq(3)
-
-		phoenix.feels_emotion(:joyful)
-		expect(phoenix.emotional_tracker[:joyful]).to eq(1)
 		expect(phoenix.releases_tear?).to be false
 	end
 
-	xit "feels joyful when being near a human" do
+	it "bursts into flames and is born again after feeling the same emotion 5 times" do
+		phoenix = Phoenix.new("Bennu")
+		5.times { phoenix.feels_emotion(:joyful) }
+		expect(phoenix.emotional_awareness).to eq({})
+		expect(phoenix.color).to eq("golden")
+		expect(phoenix.mood).to eq("stoic")
 	end
 
+	it "pharaohs have names, reputations, and a dynastic period" do
+		narmer = Pharaoh.new("Narmer", "The Unifer", "3100 BCE")
+		khufu = Pharaoh.new("Khufu", "The Builder", "3150 BCE")
+		tutankhamun = Pharaoh.new("Tutankhamun", "The Child", "1500 BCE")
 
-	xit "when that human is sick it's tear can heal the human" do
+		expect(narmer.name).to eq("Narmer")
+		expect(narmer.reputation).to eq("The Unifer")
+		expect(narmer.dynastic_period).to eq("3100 BCE")
+
+		expect(khufu.name).to eq("Khufu")
+		expect(khufu.reputation).to eq("The Builder")
+		expect(khufu.dynastic_period).to eq("3150 BCE")
+
+		expect(tutankhamun.name).to eq("Tutankhamun")
+		expect(tutankhamun.reputation).to eq("The Child")
+		expect(tutankhamun.dynastic_period).to eq("1500 BCE")
 	end
 
-	xit "spends time with humans" do
+	it "pharaohs can check if they are health" do
+		narmer = Pharaoh.new("Narmer", "The Unifer", "3100 BCE")
+		khufu = Pharaoh.new("Khufu", "The Builder", "3150 BCE")
+		tutankhamun = Pharaoh.new("Tutankhamun", "The Child", "1500 BCE")
+
+		expect(narmer.healthy?).to eq(true)
+		expect(khufu.healthy?).to eq(true)
+		expect(tutankhamun.healthy?).to eq(true)
 	end
 
+	it "pharaohs age and become unhealth by the age of 18" do
+		narmer = Pharaoh.new("Narmer", "The Unifer", "3100 BCE")
+		
+		narmer.age(10)
+		expect(narmer.healthy?).to eq(true)
+		
+		narmer.age(18)
+		expect(narmer.healthy?).to eq(false)
+
+		narmer.age(30)
+		expect(narmer.healthy?).to eq(false)
+
+	end
+
+	it "feels an emotion when observing the action of a pharaoh" do
+		phoenix = Phoenix.new("Bennu")
+		khufu = Pharaoh.new("Khufu", "The Builder", "3150 BCE")
+
+		khufu.takes_action(:fascination)
+		expect(phoenix.emotional_awareness[:fascination]).to eq(1)
+	end
+
+	it "feels an emotion when a pharaoh takes an action" do
+		phoenix = Phoenix.new("Bennu")
+		khufu = Pharaoh.new("Khufu", "The Builder", "3150 BCE")
+
+		3.times { khufu.takes_action(:fascination) }
+		expect(phoenix.releases_tear?).to eq(true)
+	end
+
+	it "the unhealthy pharaoh become healthy after receiving a phoenix tear" do
+		phoenix = Phoenix.new("Bennu")
+		tutankhamun = Pharaoh.new("Tutankhamun", "The Child", "1500 BCE")
+		
+		expect(tutankhamun.healthy?).to eq(true)
+		
+		tutankhamun.age(18)
+		expect(tutankhamun.healthy?).to eq(false)
+
+		3.times { tutankhamun.takes_action(:astonishment) }
+		expect(phoenix.releases_tear?).to eq(true)
+		expect(tutankhamun.healthy?).to eq(true)
+	end
+
+	it "when a pharaoh dies the phoenix feels sorrow 5 times, bursts into flames, and is reborn" do
+		phoenix = Phoenix.new("Bennu")
+		tutankhamun = Pharaoh.new("Tutankhamun", "The Child", "1500 BCE")
+		
+		tutankhamun.age(19)
+		expect(tutankhamun.alive?).to eq(true)
+
+		tutankhamun.dies
+		expect(tutankhamun.alive?).to eq(false)
+
+		expect(phoenix.emotional_awareness).to eq({})
+		expect(phoenix.color).to eq("golden")
+		expect(phoenix.mood).to eq("stoic")
+	end
 end
