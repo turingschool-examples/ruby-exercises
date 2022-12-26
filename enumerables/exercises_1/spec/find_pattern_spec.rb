8ec9abc527700bebf4b3d1a6c649dcd41494c7d1
Melony Franchini <meltravelz@gmail.com>
2022-12-26 11:01:06 -0700

    Add enumberable find

--- enumerables/exercises_1/spec/find_pattern_spec.rb
+++ enumerables/exercises_1/spec/find_pattern_spec.rb
@@ -12,61 +12,121 @@ RSpec.describe 'find pattern' do
     expect(found).to eq("unicorn")
   end
 
-  xit 'no waldo' do
+ it 'no waldo' do
     words = ["scarf", "sandcastle", "flag", "pretzel", "crow", "key"]
     found = nil
     words.each do |word|
-      # Your code goes here
+      if word == "waldo"
+        found = word
+      end
     end
     expect(found).to eq(nil)
   end
 
-  xit 'found waldo' do
+  it 'found waldo' do
     words = ["noise", "dog", "fair", "house", "waldo", "bucket", "fish"]
     found = nil
-    # Your code goes here
+    words.each do |word|
+      if word == "waldo"
+        found = word
+      end
+    end
     expect(found).to eq("waldo")
   end
 
-  xit 'no three letter words' do
+  it 'no three letter words' do
     words = ["piglet", "porridge", "bear", "blueberry"]
-    # Your code goes here
+    found = nil
+
+    words.each do |word|
+      if word.length == 3
+        found = word
+      end
+    end
+
     expect(found).to eq(nil)
   end
 
-  xit 'finds 13' do
+  it 'finds 13' do
     numbers = [2, 13, 19, 8, 3, 27]
-    # Your code goes here
+    found = nil
+
+    numbers.each do |num|
+      if num == 13
+        found = num
+      end
+    end
+
     expect(found).to eq(13)
   end
 
-  xit 'first even number' do
+  it 'first even number' do
     numbers = [3, 7, 13, 11, 10, 2, 17]
-    # Your code goes here
+    found = nil
+
+    numbers.each do |num|
+      if num.even? == true
+        found = num
+        break
+      end
+    end
+
     expect(found).to eq(10)
   end
 
-  xit 'first multiple of 3' do
+  it 'first multiple of 3' do
     numbers = [2, 8, 9, 27, 24, 5]
-    # Your code goes here
+    found = nil
+
+    numbers.each do |num|
+      if num % 3 == 0
+        found = num
+        break
+      end
+    end
+
     expect(found).to eq(9)
   end
 
-  xit 'first word starting with q' do
+  it 'first word starting with q' do
     words = ["weirdo", "quill", "fast", "quaint", "quitter", "koala"]
-    # Your code goes here
+    found = nil
+
+    words.each do |word|
+      if word.start_with?("q")
+        found = word
+        break
+      end
+    end
+
     expect(found).to eq("quill")
   end
 
-  xit 'first word ending with er' do
+  it 'first word ending with er' do
     words = ["biggest", "pour", "blight", "finger", "pie", "border"]
-    # Your code goes here
+    found = nil
+
+    words.each do |word|
+      if word.end_with?("er")
+        found = word
+        break
+      end
+    end
+
     expect(found).to eq("finger")
   end
 
-  xit 'first number greater than 20' do
+  it 'first number greater than 20' do
     numbers = [1, 8, 19, 21, 29, 31, 34]
-    # Your code goes here
+    found = nil
+
+    numbers.each do |num|
+      if num > 20
+        found = num
+        break
+      end
+    end
+    
     expect(found).to eq(21)
   end
 end
--- enumerables/exercises_1/spec/find_spec.rb
+++ enumerables/exercises_1/spec/find_spec.rb
@@ -2,65 +2,101 @@ RSpec.describe 'find test' do
 
   it 'first seven letter word' do
     words = ["capricious", "berry", "unicorn", "bag", "apple", "festering", "pretzel", "pencil"]
+    
     found = words.find do |word|
       word.length == 7
     end
+
     expect(found).to eq("unicorn")
   end
 
-  xit 'no waldo' do
+  it 'no waldo' do
     words = ["scarf", "sandcastle", "flag", "pretzel", "crow", "key"]
+    
     found = words.find do |word|
-      # Your code goes here
+      word == "waldo"
     end
+
     expect(found).to eq(nil)
   end
 
-  xit 'found waldo' do
+  it 'found waldo' do
     words = ["noise", "dog", "fair", "house", "waldo", "bucket", "fish"]
-    # Your code goes here
+    
+    found = words.find do |word|
+      word == "waldo"
+    end
+
     expect(found).to eq("waldo")
   end
 
-  xit 'no three letter words' do
+  it 'no three letter words' do
     words = ["piglet", "porridge", "bear", "blueberry"]
-    # Your code goes here
+   
+    found = words.find do |word|
+      word.length == 3
+    end
+
     expect(found).to eq(nil)
   end
 
-  xit 'find 13' do
+  it 'find 13' do
     numbers = [2, 13, 19, 8, 3, 27]
-    # Your code goes here
+    
+    found = numbers.find do |num|
+      num == 13
+    end 
+
     expect(found).to eq(13)
   end
 
-  xit 'find first even number' do
+  it 'find first even number' do
     numbers = [3, 7, 13, 11, 10, 2, 17]
-    # Your code goes here
+    
+    found = numbers.find do |num|
+      num.even?
+    end
+
     expect(found).to eq(10)
   end
 
-  xit 'first multiple of 3' do
+  it 'first multiple of 3' do
     numbers = [2, 8, 9, 27, 24, 5]
-    # Your code goes here
+
+    found = numbers.find do |num|
+      num % 3 == 0
+    end
+    
     expect(found).to eq(9)
   end
 
-  xit 'first word starting with q' do
+  it 'first word starting with q' do
     words = ["weirdo", "quill", "fast", "quaint", "quitter", "koala"]
-    # Your code goes here
+    
+    found = words.find do |word|
+      word.start_with?("q")
+    end
+
     expect(found).to eq("quill")
   end
 
-  xit 'first word ending with er' do
+  it 'first word ending with er' do
     words = ["biggest", "pour", "blight", "finger", "pie", "border"]
-    # Your code goes here
+  
+    found = words.find do |word|
+      word.end_with?("er")
+    end
+
     expect(found).to eq("finger")
   end
 
-  xit 'first number greater than 20' do
+  it 'first number greater than 20' do
     numbers = [1, 8, 19, 21, 29, 31, 34]
-    # Your code goes here
+    
+    found = numbers.find do |num|
+      num > 20
+    end
+
     expect(found).to eq(21)
   end
 end
