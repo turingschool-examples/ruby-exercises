1e1c29d2dcb746eeedb32d217c3007ec3af4a558
Melony Franchini <meltravelz@gmail.com>
2022-12-28 15:25:49 -0700

    Add enumerables any?

--- enumerables/exercises_1/spec/any_pattern_spec.rb
+++ enumerables/exercises_1/spec/any_pattern_spec.rb
@@ -9,43 +9,57 @@ RSpec.describe 'Any Pattern Test' do
     expect(has_zero).to eq(true)
   end
 
-  xit 'does not have any zeros' do
+  it 'does not have any zeros' do
     numbers = [3, 1, 3, 2, 4, 9, 8]
     has_zero = false
     numbers.each do |number|
-      # Your code goes here
+      has_zero = true if number.zero?
     end
     expect(has_zero).to eq(false)
   end
 
-  xit 'has at least one alice' do
+  it 'has at least one alice' do
     names = ["Bill", "Bob", "Burton", "Alice", "Brandon"]
     has_alice = false
-    # Your code goes here
+    names.each do |name|
+      has_alice = true if names.include?("Alice")
+    end
     expect(has_alice).to eq(true)
   end
 
-  xit 'no alices' do
+  it 'no alices' do
     names = ["Chuck", "Charlene", "Cory", "Chris", "Carl"]
-    # Your code goes here
+    has_alice = false
+    names.each do |name|
+      has_alice = true if names.include?("Alice")
+    end
     expect(has_alice).to eq(false)
   end
 
-  xit 'has a multi word phrase' do
+  it 'has a multi word phrase' do
     phrases = ["Sure!", "OK.", "I have no idea.", "Really?Whatever."]
-    # Your code goes here
+    has_multi_word_phrase = false
+    phrases.each do |phrase|
+      has_multi_word_phrase = true if phrase.include?(" ")
+    end
     expect(has_multi_word_phrase).to eq(true)
   end
 
-  xit 'has no monkeys' do
+  it 'has no monkeys' do
     animals = ["elephant", "hippo", "jaguar", "python"]
-    # Your code goes here
+    has_monkeys = false
+    animals.each do |animal|
+      has_monkeys = true if animal.include?("monkeys")
+    end
     expect(has_monkeys).to eq(false)
   end
 
-  xit 'has no multiples of five' do
+  it 'has no multiples of five' do
     numbers = [3, 1, 3, 2, 4, 9, 8]
-    # Your code goes here
+    multiples_of_5 = false
+    numbers.each do |num|
+      multiples_of_5 = true if num % 5 == 0
+    end
     expect(multiples_of_5).to eq(false)
   end
 end
--- enumerables/exercises_1/spec/any_spec.rb
+++ enumerables/exercises_1/spec/any_spec.rb
@@ -7,41 +7,51 @@ RSpec.describe 'Any test' do
     expect(has_zero).to eq(true)
   end
 
-  xit 'does not have zeroes' do
+  it 'does not have zeroes' do
     numbers = [3, 1, 3, 2, 4, 9, 8]
     has_zero = numbers.any? do |number|
-      # Your code goes here
+      number.zero?
     end
     expect(has_zero).to eq(false)
   end
 
-  xit 'has at least one alice' do
+  it 'has at least one alice' do
     names = ["Bill", "Bob", "Burton", "Alice", "Brandon"]
-    # Your code goes here
+    has_alice = names.any? do |name|
+      name.include?("Alice")
+    end
     expect(has_alice).to eq(true)
   end
 
-  xit 'no alices' do
+  it 'no alices' do
     names = ["Chuck", "Charlene", "Cory", "Chris", "Carl"]
-    # Your code goes here
+    has_alice = names.any? do |name|
+      names.include?("Alice")
+    end
     expect(has_alice).to eq(false)
   end
 
-  xit 'has a multi word phrase' do
+  it 'has a multi word phrase' do
     phrases = ["Sure!", "OK.", "I have no idea.", "Really?Whatever."]
-    # Your code goes here
+    multi_word_phrase = phrases.any? do |phrase|
+      phrase.include?(" ") 
+    end
     expect(multi_word_phrase).to eq(true)
   end
 
-  xit 'no monkeys' do
+  it 'has no monkeys' do
     animals = ["elephant", "hippo", "jaguar", "python"]
-    # Your code goes here
+    has_monkeys = animals.any? do |animal|
+      animal.include?("monkeys")
+    end
     expect(has_monkeys).to eq(false)
   end
 
-  xit 'no multiples of five' do
+  it 'has no multiples of five' do
     numbers = [3, 1, 3, 2, 4, 9, 8]
-    # Your code goes here
+    multiples_of_5 = numbers.any? do |num|
+      num % 5 == 0
+    end
     expect(multiples_of_5).to eq(false)
   end
 end
