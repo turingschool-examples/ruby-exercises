2a51cd9815bbf371066e8967fc0a860ba76c6c7b
Sully Birashk <sullybirashk@Sullys-MBP.home>
2024-02-10 19:40:20 -0800

    Test: Hobbit Testing

--- mythical-creatures/spec/hobbit_spec.rb
+++ mythical-creatures/spec/hobbit_spec.rb
@@ -58,21 +58,29 @@ RSpec.describe Hobbit do
     expect(hobbit.adult?).to be true
   end
 
-  xit 'is old at the age of 101' do
-    # create a hobbit
-    # have hobbit age 101 years
-    # check that hobbit.old? returns true
+  it 'is old at the age of 101' do
+    hobbit = Hobbit.new('Otho')
+
+    101.times do
+      hobbit.celebrate_birthday
+    end
+    
+    expect(hobbit.old?).to be true
   end
 
-  xit 'it has the ring if its name is Frodo' do
-    # create a hobbit named Frodo
-    # create a second hobbit named Sam
-    # check that .has_ring? for Frodo returns true
-    # check that .has_ring? for Sam returns false
+  it 'it has the ring if its name is Frodo' do
+    frodo = Hobbit.new('Frodo')
+
+    sam = Hobbit.new('Sam')
+
+    expect(frodo.has_ring?).to be true
+    expect(sam.has_ring?).to be false
+
   end
 
-  xit 'they are short' do
-    # create a hobbit
-    # check that is_short? returns true
+  it 'they are short' do
+    sam = Hobbit.new('Sam')
+
+    expect(sam.is_short?).to be true
   end
 end
